040da0cb3b7cff219777dfc811a335edb2f0cf5a
Nandini <juicyfruit5600@yahoo.ca>
2022-11-26 17:10:04 -0500

    Added North American Activity

---
+++ app/src/main/java/com/example/tatsianaptushko_nandinihariprasad_comp304sec401_lab5_ex1/NorthAmerican.java
@@ -0,0 +1,14 @@
+package com.example.tatsianaptushko_nandinihariprasad_comp304sec401_lab5_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NorthAmerican extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_north_american);
+    }
+}
