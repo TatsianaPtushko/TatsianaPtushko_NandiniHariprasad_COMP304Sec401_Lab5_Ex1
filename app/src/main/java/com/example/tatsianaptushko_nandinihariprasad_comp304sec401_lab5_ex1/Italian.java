b13a7f7f81e975c706ac4f6694f0c89498a1ae57
Tatsiana <tptushko@my.centennialcollege.ca>
2022-11-26 15:26:27 -0500

    Map Activity added

--- app/src/main/java/com/example/tatsianaptushko_nandinihariprasad_comp304sec401_lab5_ex1/Italian.java
+++ app/src/main/java/com/example/tatsianaptushko_nandinihariprasad_comp304sec401_lab5_ex1/Italian.java
@@ -3,7 +3,10 @@ package com.example.tatsianaptushko_nandinihariprasad_comp304sec401_lab5_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,6 +26,37 @@ public class Italian extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_list_view, R.id.textView, restaurantsArray);
         listView.setAdapter(arrayAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+
+                switch (selectedItem) {
+                    case "Sugo":
+                        Intent intent =new Intent(Italian.this, MapsActivity.class);
+                        intent.putExtra("lat",43.65829);
+                        intent.putExtra("lng",-79.44232);
+                        intent.putExtra("title", "Sugo");
+                        startActivity(intent);
+                        break;
+                    case "Bitondo’s Pizzeria":
+                        //     Intent intent2 =new Intent(CuisineList.this, Greek.class);
+                        //     startActivity(intent2);
+                        break;
+                    case "Terroni":
+                        //     Intent intent3 =new Intent(CuisineList.this, Chinese.class);
+                        //      startActivity(intent3);
+                        break;
+
+                    case "Death in Venice":
+                        //     Intent intent3 =new Intent(CuisineList.this, Chinese.class);
+                        //      startActivity(intent3);
+                        break;
+                }
+
+
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/example/tatsianaptushko_nandinihariprasad_comp304sec401_lab5_ex1/MapsActivity.java
@@ -0,0 +1,59 @@
+package com.example.tatsianaptushko_nandinihariprasad_comp304sec401_lab5_ex1;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.tatsianaptushko_nandinihariprasad_comp304sec401_lab5_ex1.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private double lat, lng;
+    private String titleOnMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //get extras
+
+        lat = getIntent().getDoubleExtra("lat",0);
+        lng=getIntent().getDoubleExtra("lng",0);
+        titleOnMap =getIntent().getStringExtra("title");
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng restaurant = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(restaurant).title(titleOnMap));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(restaurant,14));
+    }
+}
